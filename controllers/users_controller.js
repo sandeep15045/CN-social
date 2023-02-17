19aaad2056334ae89d9100a6f714ff32b5f4e610
Sandeep Kumar <sandeep15045@gmail.com>
2023-02-17 11:22:22 +0530

    added post controller and route

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,6 @@
 module.exports.profile=function(req,res){
-    return res.end('<h1> Users profile</h1></h1>')
+    return res.end('<h1> Users profile controller 1</h1>')
+}
+module.exports.posts=function(req,res){
+    return res.send('<h1> Users post controller 2</h1>')
 }
--- routes/users.js
+++ routes/users.js
@@ -3,5 +3,7 @@ const router= express.Router();
 const userController=require('../controllers/users_controller');
 
 router.get('/profile',userController.profile);
+router.get('/posts',userController.posts);
+
 
 module.exports=router;
