a75d0fc47e03e9a170c715bef27d2123657803d6
Sandeep Kumar <sandeep15045@gmail.com>
2023-02-17 11:32:00 +0530

    settings ejs and view engine

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const port = 8000;
 
 //telling index.js to use these routes
 app.use('/',require('./routes'))
+//set up view engine
+app.set('view engine','ejs'); 
+app.set('views','./views');
+//also can provide array for views
+
 
 app.listen(port, function(err){
     if(err){
