86e46b4274188f30b16e5a5917a1b4620f8d89f1
Sandeep Kumar <sandeep15045@gmail.com>
2023-02-17 09:42:26 +0530

    created home controller

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app= express();
 const port = 8000;
 
+//telling index.js to use these routes
+app.use('/',require('./routes'))
+
 app.listen(port, function(err){
     if(err){
         console.log('Error: ',err);
