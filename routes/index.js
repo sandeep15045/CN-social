66787269310dcd11921d4add11d867509aebdb3c
Sandeep Kumar <sandeep15045@gmail.com>
2023-02-23 11:32:53 +0530

    post created and saved in db

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,6 @@ const homeController=require('../controllers/home_controller');
 router.get('/',homeController.home);
 router.use('/users',require('./users'));
 //for any further routes, access from here
-
+router.use('/posts', require('./posts'));
 
 module.exports=router;
