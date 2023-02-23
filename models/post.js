19e9b5a6ffd2db15bc5d03d3bc9164949a4ad671
Sandeep Kumar <sandeep15045@gmail.com>
2023-02-23 22:23:34 +0530

    post and comment schema

--- models/post.js
+++ models/post.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 
-
 const postSchema = new mongoose.Schema({
     content: {
         type: String,
@@ -10,8 +9,14 @@ const postSchema = new mongoose.Schema({
         type:  mongoose.Schema.Types.ObjectId,
         ref: 'User'
 
-    }
-
+    },
+    // include the array of ids of all comments in this post schema itself
+    comments: [
+        {
+            type:  mongoose.Schema.Types.ObjectId,
+            ref: 'Comment'
+        }
+    ]
 },{
     timestamps: true
 });
