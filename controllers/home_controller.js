89817691644b914f4dacccec6076000f92ae6019
Sandeep Kumar <sandeep15045@gmail.com>
2023-02-23 22:51:26 +0530

    omment.user.name, prepopulate coments user

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -9,9 +9,17 @@ module.exports.home=function(req,res){
     //     });
     // })
 
-    Post.find({}).populate('user').exec(function(err,posts){
+    Post.find({})
+    .populate('user')
+    .populate({
+        path: 'comments',
+        populate: {
+            path: 'user'
+        }
+    })
+    .exec(function(err, posts){
         return res.render('home', {
-            title: "Codeial | Posts",
+            title: "Codeial | Home",
             posts:  posts
         });
     })
