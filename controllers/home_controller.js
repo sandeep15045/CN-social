b6ac29ba0036658c567f6c72195390ea13d1c3f6
Sandeep Kumar <sandeep15045@gmail.com>
2023-02-23 20:34:08 +0530

    37.12 post saved in db and displayed but users nt

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -2,7 +2,14 @@ const Post = require('../models/post');
 
 module.exports.home=function(req,res){
 
-    Post.find({},function(err,posts){
+    // Post.find({},function(err,posts){
+    //     return res.render('home', {
+    //         title: "Codeial | Posts",
+    //         posts:  posts
+    //     });
+    // })
+
+    Post.find({}).populate('user').exec(function(err,posts){
         return res.render('home', {
             title: "Codeial | Posts",
             posts:  posts
--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -4,9 +4,10 @@ module.exports.create=function(req,res){
     Post.create({
         content:req.body.content,
         user:req.user_id
-       
+
     }, function(err, post){
         if(err){console.log('error in creating a post'); return;}
+
         return res.redirect('back');
     });
 }
