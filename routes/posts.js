bc66290b9f9cfdd38dc5fb26f5d1f0bcf1d3168d
Sandeep Kumar <sandeep15045@gmail.com>
2023-02-23 20:40:45 +0530

    12. 2 levels of check one at view and url action

--- routes/posts.js
+++ routes/posts.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 
+
 const postsController = require('../controllers/posts_controller');
 router.post('/create', passport.checkAuthentication, postsController.create);
 
